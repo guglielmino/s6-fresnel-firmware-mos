d1ff41df7853a48661c2e59fc7ade615da0fa177
Fabrizio Guglielmino <guglielmino@gmail.com>
2017-12-09 08:04:28 +0100

    Refactor CI pipeline

--- src/consts.h
+++ src/consts.h
@@ -9,7 +9,7 @@
 #define MQTT_MESSAGE_SIZE       60
 
 #if CS_PLATFORM == CS_P_ESP8266
-    #define FIRMWARE_APP_NAME "S6 Legacy Pow Module"
+    #define FIRMWARE_APPNAME "S6 Legacy Pow Module"
     #define REL_PIN         15
     #define STATUS_LED_PIN  12
     #define BUTTON_PIN       0
@@ -19,4 +19,3 @@
     #define STATUS_LED_PIN  33
     #define BUTTON_PIN      34
 #endif
-
--- src/network/topics.h
+++ src/network/topics.h
@@ -4,10 +4,6 @@
 
 #pragma once
 
-#include <cstring>
-//#include <memory>
-//#include <iostream>
-
 /* --- MQTT publish topics -- */
 #define PUB_SENS_INFO_TOPIC         "building/%s/sensors/%s/info"           // Device publish info message on this topic
 #define PUB_SENS_POWER_TOPIC        "building/%s/sensors/%s/power"          // Device publish power consume on this topic
@@ -48,18 +44,6 @@ char pubSensVoltageTopic[MAX_TOPIC_LEN];
 
 char subSwitchDevTopic[MAX_TOPIC_LEN];
 char subSwitchRoomTopic[MAX_TOPIC_LEN];
-
-
-/*
-template<typename ... Args>
-std::string string_format( const std::string& format, Args ... args )
-{
-    size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    std::unique_ptr<char[]> buf( new char[ size ] );
-    snprintf( buf.get(), size, format.c_str(), args ... );
-    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
-}
-*/
 
 void makeDeviceTopic(char *topic, int topicLen, const char *topicPattern, const char *room, const char *deviceId) {
     memset (topic, 0, topicLen);
