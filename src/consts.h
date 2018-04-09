4bce86db1e221ac6cf203380e81c54de76ba0590
Fabrizio Guglielmino <guglielmino@gmail.com>
2018-04-09 21:54:27 +0200

    Cleanup old ESP8266 unused code and calibration test

--- src/consts.h
+++ src/consts.h
@@ -8,17 +8,11 @@
 
 #define MQTT_MESSAGE_SIZE       60
 
-#if CS_PLATFORM == CS_P_ESP8266
-    #define FIRMWARE_APPNAME "S6 Legacy Pow Module"
-    #define REL_PIN         15
-    #define STATUS_LED_PIN  12
-    #define BUTTON_PIN       0
-#elif CS_PLATFORM == CS_P_ESP32
-    #define FIRMWARE_APP_NAME "S6 Fresnel Module"
-    #define REL0_PIN_SET    32
-    #define REL0_PIN_RESET  33
-    #define REL1_PIN        12
-    #define LED_RED_PIN     15
-    #define LED_GREEN_PIN   13
-    #define BUTTON_PIN      34
-#endif
+#define FIRMWARE_APP_NAME "S6 Fresnel Module"
+#define REL0_PIN_SET    32
+#define REL0_PIN_RESET  33
+#define REL1_PIN        12
+#define LED_RED_PIN     15
+#define LED_GREEN_PIN   13
+#define BUTTON_PIN      34
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,41 @@
+#include <string>
+#include <time.h>
 
-#ifndef S6TARGET
-#pragma message "S6 MAIN FIRMWARE"
-#include "s6fw-entrypoint.hpp"
-#elif S6TARGET == calibration
-#pragma message "S6 CALIBRATION FIRMWARE"
-#include "s6calib-entrypoint.hpp"
-#endif
+#include "mgos.h"
+#include "mgos_timers.h"
+#include "mgos_config.h"
 
+#include "consts.h"
+#include "factories/uartFactories.h"
+#include "globals.h"
+
+#include "setup/cron_sys.hpp"
+#include "setup/mqtt_sys.hpp"
+#include "setup/devices_sys.hpp"
+
+void sntpTimeChangeCB(int ev, void *evd, void *arg)
+{
+    (void) ev;
+    (void)arg;
+
+    publishInfoMessage();
+}
+
+enum mgos_app_init_result mgos_app_init(void) {
+    cs_log_set_level(LL_DEBUG);
+    LOG(LL_DEBUG, ("Device ID %s", settings.deviceId()));
+
+    mqtt_sys_init();
+
+    devices_sys_init();
+
+    // Start Kw/h accumulation on MCP39F511
+    startKWhCounter();
+
+    cron_sys_init();
+
+
+    mgos_event_add_handler(MGOS_EVENT_TIME_CHANGED, sntpTimeChangeCB, NULL);
+
+    return MGOS_APP_INIT_SUCCESS;
+}
